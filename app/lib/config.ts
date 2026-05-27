518f005a3e03bd590a18ba724e99b46bc836afee
mitesh-zinc <mitesh@z.inc>
2026-05-27 18:36:17 +0530

    chore: api url

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,6 @@
 export const appConfig = {
   appEnv: process.env.NEXT_PUBLIC_APP_ENV ?? "development",
   apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_BASE_URL ??
-    "https://labs-sbox.zinc.money/api/wealth",
+    process.env.NEXT_PUBLIC_API_BASE_URL,
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
 };
