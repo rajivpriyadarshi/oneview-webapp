ca59653c09947fa0829a54b379ec33b3ea4c8760
mitesh-zinc <mitesh@z.inc>
2026-05-27 18:39:05 +0530

    chore: minor change

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
   appEnv: process.env.NEXT_PUBLIC_APP_ENV ?? "development",
   apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_BASE_URL,
+    process.env.NEXT_PUBLIC_API_BASE_URL ?? "",
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
 };
