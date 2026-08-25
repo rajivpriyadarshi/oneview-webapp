8d2976703a430816d36f62dd3a85aac74d024d3e
mitesh-zinc <mitesh@z.inc>
2026-08-25 13:23:13 +0530

    chore: minor eslint change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig([
       "react-hooks/globals": "off",
       "react-hooks/immutability": "off",
       "react-hooks/incompatible-library": "off",
+      "react-hooks/preserve-manual-memoization": "off",
       "react-hooks/refs": "off",
       "react-hooks/set-state-in-effect": "off",
       "react/no-unescaped-entities": "off",
